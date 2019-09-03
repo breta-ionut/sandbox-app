ed710dd22feae846a1a1140ee0c9a03ea8a486c3
Ionut Breta <ionut.breta@zitec.com>
2019-09-04 00:20:13 +0300

    Added some autoconfiguration.

--- src/Core/DependencyInjection/KernelExtension.php
+++ src/Core/DependencyInjection/KernelExtension.php
@@ -5,7 +5,10 @@ namespace App\Core\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
 class KernelExtension extends Extension
 {
@@ -16,5 +19,12 @@ class KernelExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('kernel.yaml');
+
+        $container->registerForAutoconfiguration(ServiceSubscriberInterface::class)
+            ->addTag('container.service_subscriber');
+        $container->registerForAutoconfiguration(EventSubscriberInterface::class)
+            ->addTag('kernel.event_subscriber');
+        $container->registerForAutoconfiguration(ArgumentValueResolverInterface::class)
+            ->addTag('controller.argument_value_resolver');
     }
 }
