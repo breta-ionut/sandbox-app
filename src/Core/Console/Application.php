2c628c9d178cc4a10df282e71c648ae96d207bbd
Ionut Breta <ionut.breta@zitec.com>
2019-10-02 00:01:24 +0300

    Enhanced the Console application.

--- src/Core/Console/Application.php
+++ src/Core/Console/Application.php
@@ -2,8 +2,12 @@
 
 namespace App\Core\Console;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 class Application extends BaseApplication
@@ -30,6 +34,80 @@ class Application extends BaseApplication
         $this->kernel = $kernel;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $this->registerCommands();
+        $this->setDispatcher($this->kernel->getContainer()->get(EventDispatcherInterface::class));
+
+        return parent::doRun($input, $output);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLongVersion()
+    {
+        return parent::getLongVersion()
+            .sprintf(
+                ' (env: <comment>%s</>, debug: <comment>%s</>)',
+                $this->kernel->getEnvironment(),
+                $this->kernel->isDebug() ? 'true' : 'false'
+            );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function add(Command $command)
+    {
+        $this->registerCommands();
+
+        return parent::add($command);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get($name)
+    {
+        $this->registerCommands();
+
+        return parent::get($name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function has($name)
+    {
+        $this->registerCommands();
+
+        return parent::has($name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function find($name)
+    {
+        $this->registerCommands();
+
+        return parent::find($name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function all($namespace = null)
+    {
+        $this->registerCommands();
+
+        return parent::all($namespace);
+    }
+
     private function registerCommands(): void
     {
         if ($this->commandsRegistered) {
@@ -43,8 +121,10 @@ class Application extends BaseApplication
 
         $this->setCommandLoader($container->get(CommandLoaderInterface::class));
 
-        foreach ($container->getParameter('console.command.ids') as $id) {
-            $this->add($container->get($id));
+        if ($container->hasParameter('console.command.ids')) {
+            foreach ($container->getParameter('console.command.ids') as $id) {
+                $this->add($container->get($id));
+            }
         }
     }
 }
