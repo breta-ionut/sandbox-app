ca99492373a01f656c19220e53fe4e3b4d934225
Ionut Breta <ionut.breta@zitec.com>
2019-10-09 21:37:36 +0300

    Removed console services file.

--- src/Core/DependencyInjection/ConsoleExtension.php
+++ src/Core/DependencyInjection/ConsoleExtension.php
@@ -2,9 +2,8 @@
 
 namespace App\Core\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ConsoleExtension extends Extension
@@ -14,7 +13,7 @@ class ConsoleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('console.yaml');
+        $container->registerForAutoconfiguration(Command::class)
+            ->addTag('console.command');
     }
 }
