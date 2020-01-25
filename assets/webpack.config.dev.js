1591cfec26175dbc3210e184c19d0f5d17040d08
Ionut Breta <ionut.breta@zitec.com>
2020-01-25 14:42:15 +0200

    Enabled webpack dev-server and versioning.

--- assets/webpack.config.dev.js
+++ assets/webpack.config.dev.js
@@ -1,11 +1,23 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const ManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: [__dirname + '/js/index.js'],
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    entry: {
+        index: path.resolve(__dirname, './js/index.js')
+    },
     output: {
         path: path.resolve(__dirname, '../public/build/js'),
         filename: '[name].js'
@@ -19,6 +31,8 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new ManifestPlugin(),
     ]
 }
