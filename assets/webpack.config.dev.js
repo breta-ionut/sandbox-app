d851012cae99bb11847b2680016aee516dccb907
Ionut Breta <ionut.breta@zitec.com>
2020-01-25 17:28:13 +0200

    Enhanced webpack dev-server config.

--- assets/webpack.config.dev.js
+++ assets/webpack.config.dev.js
@@ -11,6 +11,8 @@ module.exports = {
     devServer: {
         host: '0.0.0.0',
         port: 8080,
+        contentBase: path.resolve(__dirname, '../public/build'),
+        allowedHosts: ['localhost', 'sandbox.local'],
         hot: true,
         watchOptions: {
             poll: true
