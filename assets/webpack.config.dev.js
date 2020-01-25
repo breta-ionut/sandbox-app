a5aee8be5cdb0aaa71591f6237d2e7172a5e63f7
Ionut Breta <ionut.breta@zitec.com>
2020-01-25 17:46:31 +0200

    Enhanced webpack dev-server config #2.

--- assets/webpack.config.dev.js
+++ assets/webpack.config.dev.js
@@ -12,6 +12,7 @@ module.exports = {
         host: '0.0.0.0',
         port: 8080,
         contentBase: path.resolve(__dirname, '../public/build'),
+        writeToDisk: (filename) => 'manifest.json' === filename,
         allowedHosts: ['localhost', 'sandbox.local'],
         hot: true,
         watchOptions: {
