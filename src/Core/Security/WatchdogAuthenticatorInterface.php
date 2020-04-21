b7bc3e93682f8aef9a2302e30d4d8f711d8edc79
Ionut Breta <breta.ionut@gmail.com>
2020-04-22 01:27:51 +0300

    - added the WatchdogAuthenticatorInterface

---
+++ src/Core/Security/WatchdogAuthenticatorInterface.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+interface WatchdogAuthenticatorInterface extends AuthenticationEntryPointInterface
+{
+    /**
+     * Determines if the authenticator supports the given request.
+     *
+     * @param Request $request
+     *
+     * @return bool
+     */
+    public function supports(Request $request): bool;
+
+    /**
+     * Extracts the authentication credentials from the given request.
+     *
+     * @param Request $request
+     *
+     * @return mixed A non-null value.
+     */
+    public function getCredentials(Request $request);
+
+    /**
+     * Determines the user associated to the previously extracted credentials. If null is returned (no user found), the
+     * authentication fails (an UsernameNotFoundException gets thrown further). The authentication can also be stopped
+     * by throwing an AuthenticationException directly from the method.
+     *
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     *
+     * @return UserInterface|null
+     *
+     * @throws AuthenticationException
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface;
+
+    /**
+     * Checks if the previously determined credentials are valid. If they aren't (false is returned), the authentication
+     * fails. The authentication can also be stopped by throwing an AuthenticationException.
+     *
+     * @param $credentials
+     * @param UserInterface $user
+     *
+     * @return bool
+     *
+     * @throws AuthenticationException
+     */
+    public function checkCredentials($credentials, UserInterface $user): bool;
+
+    /**
+     * Creates an authenticated token for the given user and firewall.
+     *
+     * @param UserInterface $user
+     * @param string $firewall
+     *
+     * @return WatchdogTokenInterface
+     */
+    public function createAuthenticatedToken(UserInterface $user, string $firewall): WatchdogTokenInterface;
+
+    /**
+     * Called when the authentication fails. The resulting response, if any, gets returned immediately, otherwise the
+     * request continues. Exceptions can also be thrown from this method in the idea that they will get handled later
+     * (i.e. by an exception listener).
+     *
+     * @param Request $request
+     * @param AuthenticationException $exception
+     *
+     * @return Response|null
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response;
+
+    /**
+     * Called when the authentication succeeds. The resulting response, if any, gets returned immediately, otherwise the
+     * request continues.
+     *
+     * @param Request $request
+     * @param WatchdogTokenInterface $token
+     *
+     * @return Response|null
+     */
+    public function onAuthenticationSuccess(Request $request, WatchdogTokenInterface $token): ?Response;
+}
