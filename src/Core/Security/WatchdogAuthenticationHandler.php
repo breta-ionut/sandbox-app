3b5c06788775ea02fd90a6e51923761443ac19b1
Ionut Breta <breta.ionut@gmail.com>
2020-04-26 22:14:21 +0300

    - added the WatchdogAuthenticationHandler

---
+++ src/Core/Security/WatchdogAuthenticationHandler.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Security;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+use Symfony\Component\Security\Http\Session\SessionAuthenticationStrategyInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class WatchdogAuthenticationHandler
+{
+    private TokenStorageInterface $tokenStorage;
+    private EventDispatcherInterface $eventDispatcher;
+
+    /**
+     * @var string[]
+     */
+    private array $statelessFirewalls;
+
+    private SessionAuthenticationStrategyInterface $sessionAuthenticationStrategy;
+    private LoggerInterface $logger;
+
+    /**
+     * @param TokenStorageInterface                  $tokenStorage
+     * @param EventDispatcherInterface               $eventDispatcher
+     * @param string[]                               $statelessFirewalls
+     * @param SessionAuthenticationStrategyInterface $sessionAuthenticationStrategy
+     * @param LoggerInterface|null                   $logger
+     */
+    public function __construct(
+        TokenStorageInterface $tokenStorage,
+        EventDispatcherInterface $eventDispatcher,
+        array $statelessFirewalls,
+        SessionAuthenticationStrategyInterface $sessionAuthenticationStrategy,
+        LoggerInterface $logger = null
+    ) {
+        $this->tokenStorage = $tokenStorage;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->statelessFirewalls = $statelessFirewalls;
+        $this->sessionAuthenticationStrategy = $sessionAuthenticationStrategy;
+        $this->logger = $logger ?? new NullLogger();
+    }
+
+    /**
+     * @param WatchdogTokenInterface $token
+     * @param Request                $request
+     */
+    public function authenticateWithToken(WatchdogTokenInterface $token, Request $request): void
+    {
+        if (!\in_array($token->getFirewall(), $this->statelessFirewalls, true) && $request->hasPreviousSession()) {
+            $this->sessionAuthenticationStrategy->onAuthentication($request, $token);
+        }
+
+        $this->tokenStorage->setToken($token);
+
+        $event = new InteractiveLoginEvent($request, $token);
+        $this->eventDispatcher->dispatch($event, SecurityEvents::INTERACTIVE_LOGIN);
+    }
+
+    /**
+     * @param UserInterface                  $user
+     * @param string                         $firewall
+     * @param WatchdogAuthenticatorInterface $authenticator
+     * @param Request                        $request
+     *
+     * @return Response|null
+     */
+    public function authenticateWithUser(
+        UserInterface $user,
+        string $firewall,
+        WatchdogAuthenticatorInterface $authenticator,
+        Request $request
+    ): ?Response {
+        $token = $authenticator->createAuthenticatedToken($user, $firewall);
+
+        $this->authenticateWithToken($token, $request);
+
+        return $authenticator->onAuthenticationSuccess($request, $token);
+    }
+}
