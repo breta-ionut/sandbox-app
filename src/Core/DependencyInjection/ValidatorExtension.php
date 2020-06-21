a609f607212b814313df7ec6c5b17289b43a5c2a
Ionut Breta <breta.ionut@gmail.com>
2020-06-21 18:09:52 +0300

    - tweaks to ValidatorExtension and SerializerExtension

--- src/Core/DependencyInjection/ValidatorExtension.php
+++ src/Core/DependencyInjection/ValidatorExtension.php
@@ -7,9 +7,12 @@ namespace App\Core\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Symfony\Component\Validator\Constraints\EmailValidator;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
+use Symfony\Component\Validator\ObjectInitializerInterface;
 use Symfony\Component\Validator\ValidatorBuilder;
 
 class ValidatorExtension extends ConfigurableExtension
@@ -24,11 +27,22 @@ class ValidatorExtension extends ConfigurableExtension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('validator.yaml');
 
-        $container->getDefinition(ValidatorBuilder::class)
-            ->addMethodCall('addYamlMapping', [$this->getMappingFiles($container, $mergedConfig['mapping_dir'])]);
+        $validatorBuilder = $container->getDefinition(ValidatorBuilder::class);
+
+        $validatorBuilder->addMethodCall(
+            'addYamlMapping',
+            [$this->getMappingFiles($container, $mergedConfig['mapping_dir'])]
+        );
+        if (!$container->getParameter('kernel.debug')) {
+            $validatorBuilder->addMethodCall('setMappingCache', [new Reference('validator.mapping.cache')]);
+        }
 
         $container->getDefinition(EmailValidator::class)
             ->setArgument('$defaultMode', $mergedConfig['email_validation_mode']);
+
+        $container->registerForAutoconfiguration(ConstraintValidatorInterface::class)
+            ->addTag('validator.constraint_validator');
+        $container->registerForAutoconfiguration(ObjectInitializerInterface::class)->addTag('validator.initializer');
     }
 
     /**
