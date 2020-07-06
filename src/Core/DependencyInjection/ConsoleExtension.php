f567c551396c24c8953b3be5d0da0698a5c1e85c
Ionut Breta <breta.ionut@gmail.com>
2020-07-07 00:29:36 +0300

    - added logging for console and http

--- src/Core/DependencyInjection/ConsoleExtension.php
+++ src/Core/DependencyInjection/ConsoleExtension.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Core\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ConsoleExtension extends Extension
@@ -15,6 +17,9 @@ class ConsoleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('console.yaml');
+
         $container->registerForAutoconfiguration(Command::class)
             ->addTag('console.command');
     }
