b3dcb0c72e5182fc0a635064f4e5d585783bda45
Ionut Breta <breta.ionut@gmail.com>
2020-09-13 00:32:02 +0300

    - added user register API method

--- src/User/Controller/UserController.php
+++ src/User/Controller/UserController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\User\Controller;
 
+use App\Api\Exception\ValidationException;
 use App\Core\Controller\AbstractController;
+use App\User\Model\User;
+use App\User\User\UserManager;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
@@ -16,6 +20,28 @@ class UserController extends AbstractController
      */
     public function login(UserInterface $user): UserInterface
     {
+        return $user;
+    }
+
+    /**
+     * @param User               $user
+     * @param ValidatorInterface $validator
+     * @param UserManager        $userManager
+     *
+     * @return User
+     *
+     * @throws ValidationException
+     */
+    public function register(User $user, ValidatorInterface $validator, UserManager $userManager): User
+    {
+        $violations = $validator->validate($user);
+        if (0 !== \count($violations)) {
+            throw new ValidationException($violations);
+        }
+
+        $userManager->create($user);
+        $userManager->authenticate($user);
+
         return $user;
     }
 }
---
+++ src/User/User/UserManager.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\User;
+
+use App\User\Model\User;
+use App\User\Security\Authenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+
+class UserManager
+{
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+    private EntityManagerInterface $entityManager;
+    private UserAuthenticatorInterface $userAuthenticator;
+    private Authenticator $authenticator;
+    private RequestStack $requestStack;
+
+    /**
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     * @param EntityManagerInterface       $entityManager
+     * @param UserAuthenticatorInterface   $userAuthenticator
+     * @param Authenticator                $authenticator
+     * @param RequestStack                 $requestStack
+     */
+    public function __construct(
+        UserPasswordEncoderInterface $userPasswordEncoder,
+        EntityManagerInterface $entityManager,
+        UserAuthenticatorInterface $userAuthenticator,
+        Authenticator $authenticator,
+        RequestStack $requestStack
+    ) {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+        $this->entityManager = $entityManager;
+        $this->userAuthenticator = $userAuthenticator;
+        $this->authenticator = $authenticator;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function create(User $user): void
+    {
+        $password = $this->userPasswordEncoder->encodePassword($user, $user->getPlainPassword());
+        $user->setPassword($password);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param User $user
+     */
+    public function authenticate(User $user): void
+    {
+        $this->userAuthenticator->authenticateUser(
+            $user,
+            $this->authenticator,
+            $this->requestStack->getCurrentRequest()
+        );
+    }
+}
