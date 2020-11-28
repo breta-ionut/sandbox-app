fdada524ef24d3134e29dc8d2c75dc4f24c81b13
Ionut Breta <breta.ionut@gmail.com>
2020-11-29 00:55:17 +0200

    - markup for login form

--- assets/js/views/Login.vue
+++ assets/js/views/Login.vue
@@ -1,3 +1,23 @@
 <script src="./Login.js"></script>
 
-<template></template>
+<template>
+    <div class="flex justify-center py-16">
+        <form class="px-8 py-4 bg-white border rounded shadow-md">
+            <div class="mb-4">
+                <label for="email" class="block mb-2 text-md text-gray-700 font-bold">Email</label>
+
+                <input type="text" name="email" id="email" class="px-3 py-2 border rounded shadow text-gray-600" placeholder="john.doe@example.com" />
+            </div>
+
+            <div>
+                <label for="password" class="block mb-2 text-md text-gray-700 font-bold">Password</label>
+
+                <input type="password" name="password" id="password" class="px-3 py-2 border rounded shadow text-gray-600" placeholder="********" />
+            </div>
+
+            <div class="mt-8">
+                <input type="submit" name="submit" value="Submit" class="px-4 py-2 bg-blue hover:bg-blue-dark rounded text-white font-bold" />
+            </div>
+        </form>
+    </div>
+</template>
