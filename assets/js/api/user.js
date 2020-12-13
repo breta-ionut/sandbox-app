6a66203705d0cd193f697085c41fdadf5c84b131
Ionut Breta <breta.ionut@gmail.com>
2020-12-13 18:44:54 +0200

    - small fix in user api

--- assets/js/api/user.js
+++ assets/js/api/user.js
@@ -2,10 +2,10 @@ import axios from '../axios.js'
 
 export default {
     async get() {
-        return axios.get('/user')
+        return axios.get('/user').then((response) => response.data)
     },
 
     async login(credentials) {
-        return axios.post('/user/login', credentials)
+        return axios.post('/user/login', credentials).then((response) => response.data)
     },
 }
