0cfa4a4dcfabac5c87e134838032204f93fc5366
Ionut Breta <breta.ionut@gmail.com>
2020-12-15 00:26:26 +0200

    - added errorsInterceptor

--- assets/js/api/errorsInterceptor.js
+++ assets/js/api/errorsInterceptor.js
@@ -1,1 +1,21 @@
-export
+import Error from '../models/Error.js'
+import ValidationError from '../models/ValidationError.js'
+
+const ERROR_TITLE_UNKNOWN = 'An error occurred.'
+
+const ERROR_CODE_UNKNOWN = 100
+const ERROR_CODE_VALIDATION = 103
+
+export default function (error) {
+    if (!error.response?.data?.code) {
+        throw new Error(ERROR_TITLE_UNKNOWN, ERROR_CODE_UNKNOWN, null)
+    }
+
+    switch (error.response.data.code) {
+        case ERROR_CODE_VALIDATION:
+            throw ValidationError.fromApiResponseData(error.response.data)
+
+        default:
+            throw Error.fromApiResponseData(error.response.data)
+    }
+}
