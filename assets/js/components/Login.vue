77860bc0338fb077af2d01f05713f5da0ae62237
Ionut Breta <breta.ionut@gmail.com>
2020-12-18 01:15:32 +0200

    - display errors

--- assets/js/components/Login.vue
+++ assets/js/components/Login.vue
@@ -7,12 +7,16 @@
                 <label for="username" class="block mb-2 text-md text-gray-700 font-bold">Email</label>
 
                 <input type="email" name="username" id="username" class="px-3 py-2 border rounded shadow text-gray-600" placeholder="john.doe@example.com" v-model="username" />
+
+                <p class="mt-3 text-sm text-red-500" v-for="error in errors.fields.username">{{ error }}</p>
             </div>
 
             <div>
                 <label for="password" class="block mb-2 text-md text-gray-700 font-bold">Password</label>
 
                 <input type="password" name="password" id="password" class="px-3 py-2 border rounded shadow text-gray-600" placeholder="********" v-model="password" />
+
+                <p class="mt-3 text-sm text-red-500" v-for="error in errors.fields.password">{{ error }}</p>
             </div>
 
             <div class="mt-8">
