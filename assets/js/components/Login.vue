098a2bbd7779c2b0f19246ff6970558ca67fa44b
Ionut Breta <breta.ionut@gmail.com>
2020-12-19 18:21:56 +0200

    - display global errors

--- assets/js/components/Login.vue
+++ assets/js/components/Login.vue
@@ -3,10 +3,14 @@
 <template>
     <div class="flex justify-center py-16">
         <form class="px-8 py-4 bg-white border rounded shadow-md" @submit.prevent="login">
+            <div class="mt-2 mb-6 p-2 bg-red-100 border border-red-500 rounded text-sm text-red-500 text-center" v-if="errors.global">
+                <p v-for="error in errors.global">{{ error }}</p>
+            </div>
+
             <div class="mb-4">
                 <label for="username" class="block mb-2 text-md text-gray-700 font-bold">Email</label>
 
-                <input type="email" name="username" id="username" class="px-3 py-2 border rounded shadow text-gray-600" placeholder="john.doe@example.com" required v-model="username" />
+                <input type="email" name="username" id="username" class="px-3 py-2 border rounded shadow text-gray-600" :class="{'border-red-500': errors.fields.username}" placeholder="john.doe@example.com" required v-model="username" />
 
                 <p class="mt-3 text-sm text-red-500" v-for="error in errors.fields.username">{{ error }}</p>
             </div>
@@ -14,7 +18,7 @@
             <div>
                 <label for="password" class="block mb-2 text-md text-gray-700 font-bold">Password</label>
 
-                <input type="password" name="password" id="password" class="px-3 py-2 border rounded shadow text-gray-600" placeholder="********" required v-model="password" />
+                <input type="password" name="password" id="password" class="px-3 py-2 border rounded shadow text-gray-600" :class="{'border-red-500': errors.fields.password}" placeholder="********" required v-model="password" />
 
                 <p class="mt-3 text-sm text-red-500" v-for="error in errors.fields.password">{{ error }}</p>
             </div>
