7e677301f860c76e118d5e949e456fd84cc32c46
Ionut Breta <breta.ionut@gmail.com>
2021-01-03 16:38:01 +0200

    - changed DoctrineMigrationsConfiguration for Doctrine Migrations v3

--- src/Core/DependencyInjection/DoctrineMigrationsConfiguration.php
+++ src/Core/DependencyInjection/DoctrineMigrationsConfiguration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Core\DependencyInjection;
 
+use Doctrine\Migrations\Configuration\Configuration;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -18,11 +19,11 @@ class DoctrineMigrationsConfiguration implements ConfigurationInterface
         $root = $treeBuilder->getRootNode();
 
         $root
-            ->fixXmlConfig('migration_path')
+            ->fixXmlConfig('migrations_path')
             ->fixXmlConfig('migration')
 
             ->children()
-                ->arrayNode('migration_paths')
+                ->arrayNode('migrations_paths')
                     ->useAttributeAsKey('namespace')
                     ->normalizeKeys(false)
                     ->defaultValue(['AppMigrations' => '%kernel.project_dir%/src/Migrations'])
@@ -33,6 +34,59 @@ class DoctrineMigrationsConfiguration implements ConfigurationInterface
                 ->arrayNode('migrations')
                     ->scalarPrototype()
                         ->cannotBeEmpty()
+                    ->end()
+                ->end()
+
+                ->enumNode('organize_migrations')
+                    ->values([
+                        false,
+                        Configuration::VERSIONS_ORGANIZATION_BY_YEAR,
+                        Configuration::VERSIONS_ORGANIZATION_BY_YEAR_AND_MONTH,
+                    ])
+                    ->defaultFalse()
+                    ->treatFalseLike(Configuration::VERSIONS_ORGANIZATION_NONE)
+                ->end()
+
+                ->scalarNode('custom_template')
+                    ->defaultNull()
+                ->end()
+
+                ->booleanNode('all_or_nothing')
+                    ->defaultFalse()
+                ->end()
+
+                ->booleanNode('check_database_platform')
+                    ->defaultTrue()
+                ->end()
+
+                ->arrayNode('storage')
+                    ->addDefaultsIfNotSet()
+
+                    ->children()
+                        ->scalarNode('table_name')
+                            ->cannotBeEmpty()
+                            ->defaultValue('migration_versions')
+                        ->end()
+
+                        ->scalarNode('version_column_name')
+                            ->cannotBeEmpty()
+                            ->defaultValue('version')
+                        ->end()
+
+                        ->integerNode('version_column_length')
+                            ->min(1)
+                            ->defaultValue(191)
+                        ->end()
+
+                        ->scalarNode('executed_at_column_name')
+                            ->cannotBeEmpty()
+                            ->defaultValue('executed_at')
+                        ->end()
+
+                        ->scalarNode('execution_time_column_name')
+                            ->cannotBeEmpty()
+                            ->defaultValue('execution_time')
+                        ->end()
                     ->end()
                 ->end()
             ->end();
