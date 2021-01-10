42c8696de6ed62c2bca2ea76caf689ac0df7dfd2
Ionut Breta <breta.ionut@gmail.com>
2021-01-10 20:54:26 +0200

    - added authorizationInterceptor

---
+++ assets/js/api/authorizationInterceptor.js
@@ -0,0 +1,7 @@
+import store from '../store/index.js'
+
+export default config => {
+    if (store.getters.isTokenAvailable) {
+        config.headers.Authorization = `Token ${store.getters.token}`
+    }
+}
--- assets/js/axios.js
+++ assets/js/axios.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+import authorizationInterceptor from './api/authorizationInterceptor.js'
 import errorInterceptor from './api/errorInterceptor.js'
 
 axios.defaults.baseURL = API_BASE_URL
 axios.defaults.headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
+axios.interceptors.request.use(authorizationInterceptor)
 axios.interceptors.response.use(null, errorInterceptor)
 
 export default axios
