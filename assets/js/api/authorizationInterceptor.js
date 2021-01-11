46023bc4a1fc7e847dcedf027c2a27f54299c725
Ionut Breta <breta.ionut@gmail.com>
2021-01-12 00:13:14 +0200

    - some changes to user store

--- assets/js/api/authorizationInterceptor.js
+++ assets/js/api/authorizationInterceptor.js
@@ -1,8 +1,8 @@
 import store from '../store/index.js'
 
 export default config => {
-    if (store.getters['user/isTokenAvailable']) {
-        config.headers.Authorization = `Token ${store.getters['user/token']}`
+    if (store.getters['user/hasToken']) {
+        config.headers.Authorization = `Token ${store.state['user/token']}`
     }
 
     return config
