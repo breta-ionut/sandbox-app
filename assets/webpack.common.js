46308da5d59912d342467b1e99b9685e9e62b123
Ionut Breta <breta.ionut@gmail.com>
2021-01-14 00:02:31 +0200

    - small fix

--- assets/webpack.common.js
+++ assets/webpack.common.js
@@ -50,7 +50,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                use: 'asset/resource',
+                type: 'asset/resource',
             },
         ],
     },
