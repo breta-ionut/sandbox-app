863d6d05bbfc8119374b180d2b114986f5a90939
Ionut Breta <breta.ionut@gmail.com>
2021-01-26 00:10:13 +0200

    - refactor authenticationRequiredInterceptor

--- assets/js/api/authenticationRequiredInterceptor.js
+++ assets/js/api/authenticationRequiredInterceptor.js
@@ -1,10 +1,15 @@
 import errorCodes from './errorCodes.js'
 import ApiError from '../errors/ApiError.js'
+import router from '../router.js'
 import store from '../store/index.js'
 
 export default error => {
-    if (error instanceof ApiError && error.getCode() === errorCodes.AUTHENTICATION_REQUIRED) {
-        store.commit('user/requireAuthentication')
+    if (error instanceof ApiError
+        && error.getCode() === errorCodes.AUTHENTICATION_REQUIRED
+        && !error.getOriginal().config?.disableAutoLogout
+    ) {
+        store.commit('user/logout')
+        router.push({name: 'login'})
     }
 
     throw error
