acc76b3cfaa594e1c2dd2372a895c6cc301a5439
Ionut Breta <breta.ionut@gmail.com>
2021-01-29 23:58:37 +0200

    - fixed authorizationInterceptor

--- assets/js/api/authorizationInterceptor.js
+++ assets/js/api/authorizationInterceptor.js
@@ -2,7 +2,7 @@ import store from '../store/index.js'
 
 export default config => {
     if (store.getters['user/hasToken']) {
-        config.headers.Authorization = `Token ${store.state['user/token']}`
+        config.headers.Authorization = `Token ${store.state.user.token}`
     }
 
     return config
