27064a9005bcd6d2ae906e40d27b4a833de47ccc
Ionut Breta <breta.ionut@gmail.com>
2021-02-01 22:41:25 +0200

    - integrated user loader

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -1,5 +1,11 @@
 <style src="../../css/index.css"></style>
 
+<script src="./App.js"></script>
+
 <template>
     <router-view></router-view>
+
+    <div class="flex justify-center py-12" v-if="userLoading">
+        <div class="main-loader"></div>
+    </div>
 </template>
