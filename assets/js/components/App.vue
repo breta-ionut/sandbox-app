fcc8d02facb4f0efb86a5d8f603cb5f2fa614df3
Ionut Breta <breta.ionut@gmail.com>
2021-02-01 22:47:14 +0200

    - changed styling for user loader

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -5,7 +5,7 @@
 <template>
     <router-view></router-view>
 
-    <div class="flex justify-center py-12" v-if="userLoading">
+    <div class="flex justify-center pt-40" v-if="userLoading">
         <div class="main-loader"></div>
     </div>
 </template>
