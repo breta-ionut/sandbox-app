f834179441212b31d0bdee7b89acd1379bbfb33a
Ionut Breta <breta.ionut@gmail.com>
2021-02-07 01:45:48 +0200

    - fixed Navbar

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bg-gray-300">
-        <div class="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
                 <div class="flex-1 flex items-center sm:items-stretch justify-center sm:justify-start">
                     <div class="flex-shrink-0 flex items-center">
