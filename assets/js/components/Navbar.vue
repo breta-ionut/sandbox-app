6ae5468ed9e3dcfbaf0b9c8ac8d773c94bf4dcb1
Ionut Breta <breta.ionut@gmail.com>
2021-02-07 01:30:04 +0200

    - fixed Navbar

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -7,19 +7,19 @@
                         <img class="block w-auto h-8" src="../../images/logo.png" alt="Random Tools" />
                     </div>
                 </div>
-            </div>
 
-            <div class="absolute sm:static inset-y-0 sm:inset-auto right-0 flex items-center sm:ml-6 pr-2 sm:pr-0">
-                <div class="relative ml-3">
-                    <div>
-                        <button id="user-menu" class="flex bg-gray-300 rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" aria-haspopup="true">
-                            <span class="sr-only">Open user menu</span>
-                            <span class="w-8 h-8 rounded-full fa fa-user"></span>
-                        </button>
-                    </div>
+                <div class="absolute sm:static inset-y-0 sm:inset-auto right-0 flex items-center sm:ml-6 pr-2 sm:pr-0">
+                    <div class="relative ml-3">
+                        <div>
+                            <button id="user-menu" class="flex bg-gray-300 rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300" aria-haspopup="true">
+                                <span class="sr-only">Open user menu</span>
+                                <span class="w-8 h-8 rounded-full fa fa-user"></span>
+                            </button>
+                        </div>
 
-                    <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
+                        <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
+                        </div>
                     </div>
                 </div>
             </div>
