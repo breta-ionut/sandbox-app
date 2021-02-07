d95f7939c57009e02b68fbf74889a545256c208e
Ionut Breta <breta.ionut@gmail.com>
2021-02-08 00:07:56 +0200

    - added Navbar open/close user menu functionality

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -1,3 +1,5 @@
+<script src="./Navbar.js"></script>
+
 <template>
     <nav class="bg-white border-b border-gray-200 shadow">
         <div class="mx-auto px-2 sm:px-6 lg:px-8">
@@ -11,15 +13,17 @@
                 <div class="absolute sm:static inset-y-0 sm:inset-auto right-0 flex items-center sm:ml-6 pr-2 sm:pr-0">
                     <div class="relative ml-3">
                         <div>
-                            <button id="user-menu" class="flex bg-white rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 focus:ring-offset-white" aria-haspopup="true">
+                            <button id="user-menu" class="flex bg-white rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 focus:ring-offset-white" aria-haspopup="true" v-bind:aria-expanded="showUserMenu">
                                 <span class="sr-only">Open user menu</span>
                                 <span class="w-8 h-8 text-lg rounded-full fa fa-user"></span>
                             </button>
                         </div>
 
-                        <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
-                        </div>
+                        <transition enter-class="transform opacity-0 scale-95" enter-active-class="transition ease-out duration-100" enter-to-class="transform opacity-100 scale-100" leave-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-to-class="transform opacity-0 scale-95">
+                            <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" v-if="showUserMenu">
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
+                            </div>
+                        </transition>
                     </div>
                 </div>
             </div>
