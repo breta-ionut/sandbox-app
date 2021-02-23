3a960763e0bdadd086ec5ff215cf6480e051b31c
Ionut Breta <breta.ionut@gmail.com>
2021-02-24 01:14:18 +0200

    - added functionality to open/close navbar menu

--- assets/js/components/Navbar.js
+++ assets/js/components/Navbar.js
@@ -1,5 +1,18 @@
 export default {
     data: () => ({
+        /**
+         * @type {boolean}
+         */
         showUserMenu: false,
     }),
+
+    methods: {
+        toggleUserMenu() {
+            this.showUserMenu = !this.showUserMenu
+        },
+
+        hideUserMenu() {
+            this.showUserMenu = false
+        },
+    }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ import 'regenerator-runtime/runtime'
 
 import {createApp} from 'vue'
 import VueAxios from 'vue-axios'
+import VueClickAwayPlugin from 'vue3-click-away'
 
 import axios from './axios.js'
 import App from './pages/App.vue'
@@ -13,4 +14,5 @@ createApp(App)
     .use(router)
     .use(VueAxios, axios)
     .use(store)
+    .use(VueClickAwayPlugin)
     .mount('#app')
