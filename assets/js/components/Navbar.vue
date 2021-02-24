608b34430c0228aab7865efe7b3b3c9b0e932083
Ionut Breta <breta.ionut@gmail.com>
2021-02-24 23:30:07 +0200

    - small fix to Navbar.vue

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -10,7 +10,7 @@
                     </div>
                 </div>
 
-                <div class="absolute sm:static inset-y-0 sm:inset-auto right-0 flex items-center sm:ml-6 pr-2 sm:pr-0">
+                <div class="absolute sm:static inset-y-0 sm:inset-auto right-0 flex items-center sm:ml-6 pr-2 sm:pr-0" v-click-away="hideUserMenu">
                     <div class="relative ml-3">
                         <div>
                             <button id="user-menu" class="flex bg-white rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 focus:ring-offset-white" aria-haspopup="true" v-bind:aria-expanded="showUserMenu" @click="toggleUserMenu">
@@ -20,7 +20,7 @@
                         </div>
 
                         <transition enter-class="transform opacity-0 scale-95" enter-active-class="transition ease-out duration-100" enter-to-class="transform opacity-100 scale-100" leave-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-to-class="transform opacity-0 scale-95">
-                            <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" v-if="showUserMenu" v-click-away="hideUserMenu">
+                            <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" v-if="showUserMenu">
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
                             </div>
                         </transition>
