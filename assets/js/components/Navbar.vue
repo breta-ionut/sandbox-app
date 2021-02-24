13aeb8ed4b95e91d4fd0b6a93e710570cc890d69
Ionut Breta <breta.ionut@gmail.com>
2021-02-25 00:40:26 +0200

    - styling Navbar logout button

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -21,7 +21,7 @@
 
                         <transition enter-class="transform opacity-0 scale-95" enter-active-class="transition ease-out duration-100" enter-to-class="transform opacity-100 scale-100" leave-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-to-class="transform opacity-0 scale-95">
                             <div class="absolute right-0 w-48 mt-2 py-1 bg-white rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" v-if="showUserMenu">
-                                <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" @click.prevent="logout">Logout</a>
+                                <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" @click.prevent="logout">Logout</a>
                             </div>
                         </transition>
                     </div>
