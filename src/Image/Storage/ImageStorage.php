047e09941d30c666f882fc2871a72da4d0388043
Ionut Breta <breta.ionut@gmail.com>
2021-04-26 00:33:13 +0300

    - fixes

--- src/Image/Storage/ImageStorage.php
+++ src/Image/Storage/ImageStorage.php
@@ -8,7 +8,9 @@ use App\Common\Filesystem\PublicFilesystemOperator;
 use App\Image\Model\Image;
 use App\Image\Model\ImageContent;
 use App\Image\Style\ImageStyler;
+use League\Flysystem\Config;
 use League\Flysystem\FilesystemOperator;
+use League\Flysystem\Visibility;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ImageStorage
@@ -67,7 +69,11 @@ class ImageStorage
             $imageContent = $this->imageStyler->apply($imageContent, $style);
         }
 
-        $this->publicFilesystem->write($this->getPublicPath($image, $style), (string) $imageContent);
+        $this->publicFilesystem->write(
+            $this->getPublicPath($image, $style),
+            (string) $imageContent,
+            [Config::OPTION_VISIBILITY => Visibility::PUBLIC],
+        );
     }
 
     /**
