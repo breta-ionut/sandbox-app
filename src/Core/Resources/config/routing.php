3407e0a81a451a7773f34ad279e3a33dba67daab
Ionut Breta <breta.ionut@gmail.com>
2021-06-19 23:23:08 +0300

    - moved Routing config to PHP

--- src/Core/Resources/config/routing.php
+++ src/Core/Resources/config/routing.php
@@ -7,17 +7,24 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use App\Core\Routing\AnnotationControllerLoader;
 use App\Core\Routing\RedirectableCompiledUrlMatcher;
 use App\Core\Routing\RootLoader;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\DelegatingLoader;
 use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\Loader\AnnotationDirectoryLoader;
 use Symfony\Component\Routing\Loader\AnnotationFileLoader;
 use Symfony\Component\Routing\Loader\ContainerLoader;
 use Symfony\Component\Routing\Loader\GlobFileLoader;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
@@ -76,4 +83,20 @@ return static function (ContainerConfigurator $container): void {
             ],
             service(RequestContext::class),
         ]);
+
+    $services->alias(RouterInterface::class, Router::class);
+    $services->alias(UrlGeneratorInterface::class, Router::class);
+    $services->alias(RequestMatcherInterface::class, Router::class);
+    $services->alias(UrlMatcherInterface::class, Router::class);
+
+    $services->set(RouterListener::class)
+        ->args([
+            service(RequestMatcherInterface::class),
+            service(RequestStack::class),
+            service(RequestContext::class),
+            service(LoggerInterface::class),
+            param('kernel.project_dir'),
+            param('kernel.debug'),
+        ])
+        ->tag('kernel.event_subscriber');
 };
