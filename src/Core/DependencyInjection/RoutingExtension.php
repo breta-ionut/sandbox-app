eda0854eb060b3a120972433718e6b1ef6b4ca5e
Ionut Breta <breta.ionut@gmail.com>
2021-06-19 12:18:25 +0300

    - moved Routing config to PHP

--- src/Core/DependencyInjection/RoutingExtension.php
+++ src/Core/DependencyInjection/RoutingExtension.php
@@ -6,7 +6,7 @@ namespace App\Core\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class RoutingExtension extends Extension
@@ -14,9 +14,9 @@ class RoutingExtension extends Extension
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('routing.yaml');
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('routing.php');
     }
 }
---
+++ src/Core/Resources/config/routing.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use App\Core\Routing\AnnotationControllerLoader;
+use App\Core\Routing\RootLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Routing\Loader\AnnotationDirectoryLoader;
+use Symfony\Component\Routing\Loader\AnnotationFileLoader;
+use Symfony\Component\Routing\Loader\ContainerLoader;
+use Symfony\Component\Routing\Loader\GlobFileLoader;
+use Symfony\Component\Routing\Loader\PhpFileLoader;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+
+return static function (ContainerConfigurator $container): void {
+    $services = $container->services();
+
+    // Loaders.
+    $services->set(RootLoader::class)
+        ->args([service(PhpFileLoader::class), param('kernel.config_dir'), param('kernel.environment')])
+        ->public();
+
+    $services->set('routing.file_locator', FileLocator::class);
+
+    $services->set(AnnotationControllerLoader::class)
+        ->args([null, param('kernel.environment')]);
+
+    $services->set('routing.resolver', LoaderResolver::class);
+
+    $services->set(AnnotationDirectoryLoader::class)
+        ->args([service('routing.file_locator'), service(AnnotationControllerLoader::class)])
+        ->tag('routing.loader');
+
+    $services->set(AnnotationFileLoader::class)
+        ->args([service('routing.file_locator'), service(AnnotationControllerLoader::class)])
+        ->tag('routing.loader');
+
+    $services->set(ContainerLoader::class)
+        ->args([service('service_container'), param('kernel.environment')])
+        ->tag('routing.loader');
+
+    $services->set(GlobFileLoader::class)
+        ->args([service('routing.file_locator'), param('kernel.environment')])
+        ->tag('routing.loader');
+
+    $services->set(PhpFileLoader::class)
+        ->args([service('routing.file_locator'), param('kernel.environment')])
+        ->tag('routing.loader');
+
+    $services->set(YamlFileLoader::class)
+        ->args([service('routing.file_locator'), param('kernel.environment')])
+        ->tag('routing.loader');
+
+    $services->set('routing.loader', DelegatingLoader::class)
+        ->args([service('routing.resolver')]);
+    // End of - Loaders.
+};
