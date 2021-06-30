caa053d636b6f88a722fc4bee81deb140128191c
Ionut Breta <breta.ionut@gmail.com>
2021-07-01 00:35:12 +0300

    - some linting

--- assets/js/pages/App.js
+++ assets/js/pages/App.js
@@ -2,6 +2,9 @@ import {mapState} from 'vuex'
 
 export default {
     computed: mapState('user', {
+        /**
+         * @return {boolean}
+         */
         userLoading: state => !state.userLoaded,
     }),
 }
